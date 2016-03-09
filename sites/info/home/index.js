2f205a14b62732bcd04b98ddde97513ac647b04f
Oskar M. Grande <oskar@daruma.io>
2016-03-09 17:41:15 +0100

    Simple HTML/CSS templates with middleman approach

--- sites/info/home/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home }    = './home';
-export { default as meta }    = './meta_en.yml';
-export { default as content } = './content_en.yml';
--- sites/info/index.js
+++
@@ -1,5 +0,0 @@
-export meta     = './meta_en.yml';
-export Layout   = './layout';
-export subPages = {
-  Home: './home'
-};
