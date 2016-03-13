c2ef58a05d028c38aa7805ec4ed794d967f3231b
Oskar M. Grande <oskar@daruma.io>
2016-03-13 15:41:13 +0100

    Another pre-commit build hook test

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
+readonly BASH_PROFILE=$HOME/.bash_profile
+readonly ZSH_PROFILE=$HOME/.zshrc
+
+if [ -f $BASH_PROFILE ];
+then
+  source $BASH_PROFILE
+elif [ -f $ZSH_PROFILE ];
+then
+  source $ZSH_PROFILE
+fi
+
 echo "Path for 'bundle':"
 which bundle
 BUNDLE=$?
@@ -8,7 +19,8 @@ echo "Path for 'middleman':"
 which middleman
 MIDDLEMAN=$?
 
-if [ $BUNDLE -eq 0 ] && [ $MIDDLEMAN -eq 0 ]; then
+if [ $BUNDLE -eq 0 ] && [ $MIDDLEMAN -eq 0 ];
+then
   echo "Building templates pre-commit."
   bundle exec middleman build
   git add ./build
