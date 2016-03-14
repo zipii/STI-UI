c98d3e7f178ec9c488dcb8df8c3b99df3899402e
Oskar M. Grande <oskar@daruma.io>
2016-03-14 01:49:03 +0100

    Found solution for chosing flags, needs a bit of polishing will work on this tomorrow early in the morning

--- source/javascripts/questions.js
+++ source/javascripts/questions.js
@@ -4,8 +4,6 @@ function handleStepClick(event) {
   return true;
 }
 
-addClickHandlersForClassName(handleStepClick, 'questions-form__header__navigation__step__content');
-
 function handleExpandArrowClick(event) {
   console.log(event);
   var $clickedElement = $(event.target);
@@ -18,4 +16,38 @@ function handleExpandArrowClick(event) {
   return true;
 }
 
-addClickHandlersForClassName(handleExpandArrowClick, 'expand-arrow');
+$(document).ready(function() {
+
+  addClickHandlersForClassName(handleStepClick, 'questions-form__header__navigation__step__content');
+  addClickHandlersForClassName(handleExpandArrowClick, 'expand-arrow');
+
+  $('.selectpicker').selectpicker({
+    size: 10
+  });
+
+
+  // $('#questions-form__header__locality__country_select option').each(function() {
+  //   var $option       = $(this);
+  //   var twoLetterCode = $option.val();
+  //   var $flagIcon = $('<i/>').addClass('flag flag-' + twoLetterCode.toLowerCase());
+  //   $option.data('content', $flagIcon.outerHtml);
+  //   console.log($option.data('content'))
+  // });
+
+});
+
+// $('#questions-form__header__locality__country__select').flagStrap({
+//     buttonSize: 'btn-lg',
+//     buttonType: 'btn-primary',
+//     labelMargin: '20px',
+//     scrollable: true,
+//     scrollableHeight: '350px',
+//     onSelect: function (value, element) {
+//         alert(value);
+//         console.log(element);
+//     },
+//     placeholder: {
+//       value: "",
+//       text: "Please select a country"
+//     }
+// });
