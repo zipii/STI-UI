a2c668e47e2d69ca92218aaad315011ec6a34a8a
Oskar M. Grande <oskar@daruma.io>
2016-03-15 17:38:09 +0100

    Remove iframe resizer log output

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -1,15 +1,5 @@
 console.log('loaded home..');
 
 var iframes = iFrameResize({
-  log: true,
-  resizedCallback: function(messageData) {
-    console.log(messageData);
-  },
-  messageCallback: function(messageData) {
-    console.log(messageData);
-  },
-  closedCallback: function(messageData) {
-    console.log(messageData);
-  },
   heightCalculationMethod: 'max'
 });
