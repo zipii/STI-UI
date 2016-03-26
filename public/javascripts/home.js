d1aa467c81dbfd265a680e249c844af2d33c524d
Oskar M. Grande <oskar@daruma.io>
2016-03-26 01:46:44 +0100

    Latest build

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,12 @@
+function daysUntil(deadline) {
+  var today = new Date();
+  var millisecondsUntil = deadline.getTime() - today.getTime();
+  return Math.floor(((((millisecondsUntil / 1000) / 60) / 60) / 24));
+}
+
 $(document).ready(function() {
-  console.log('loaded home..');
+
+  var deadline = new Date('2016-03-29');
 
   var iframes = iFrameResize({
     heightCalculationMethod: 'max'
@@ -8,4 +15,11 @@ $(document).ready(function() {
   $('.carousel').carousel({
     interval: false
   });
+
+  $.get('/counter/count.json', function(counter) {
+    $('.home__counter__container__content__counter').show();
+    $('.counter-message').first().html(counter.count);
+    $('.counter-days-left').first().html(daysUntil(deadline));
+  });
+
 });
