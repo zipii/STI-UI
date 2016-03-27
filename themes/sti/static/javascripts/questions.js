814976bd7fbd594458aeaa96c36a2efbc1ea4888
Oskar M. Grande <oskar@daruma.io>
2016-03-27 23:32:27 +0200

    Cleaned up some logging messages

--- themes/sti/static/javascripts/questions.js
+++ themes/sti/static/javascripts/questions.js
@@ -26,6 +26,23 @@ function handleExpandArrowClick(event) {
   return true;
 }
 
+function registerStepHoverEvent(id, addClassName, removeClassName) {
+  $(id).hover(function() {
+    if (!$(this).hasClass('active')) {
+      $(this).find('i').addClass(addClassName);
+      $(this).find('i').removeClass(removeClassName);
+      $(this).find('i').addClass('fa-5x');
+      $(this).find('i').removeClass('fa-3x');
+    }
+  }, function() {
+    if (!$(this).hasClass('active')) {
+      $(this).find('i').addClass(removeClassName);
+      $(this).find('i').removeClass(addClassName);
+      $(this).find('i').addClass('fa-3x');
+      $(this).find('i').removeClass('fa-5x');
+    }
+  });
+}
 
 if (typeof handleLanguageSelect === 'undefined') {
   function handleLanguageSelect(event) {
@@ -41,40 +58,12 @@ if (typeof handleCountrySelect === 'undefined') {
   }
 }
 
-function registerStepHoverEvent(id, addClassName, removeClassName) {
-  $(id).hover(function() {
-    console.log('hover');
-    if (!$(this).hasClass('active')) {
-      $(this).find('i').addClass(addClassName);
-      $(this).find('i').removeClass(removeClassName);
-      $(this).find('i').addClass('fa-5x');
-      $(this).find('i').removeClass('fa-3x');
-    }
-  }, function() {
-    if (!$(this).hasClass('active')) {
-      console.log('unhover');
-      $(this).find('i').addClass(removeClassName);
-      $(this).find('i').removeClass(addClassName);
-      $(this).find('i').addClass('fa-3x');
-      $(this).find('i').removeClass('fa-5x');
-    }
-  });
-}
-
-
 $(document).ready(function() {
 
   registerStepHoverEvent('#questions-form__header__navigation__step-1', 'fa-check-square-o', 'fa-check-square');
   registerStepHoverEvent('#questions-form__header__navigation__step-2', 'fa-pencil-square-o', 'fa-pencil-square');
   registerStepHoverEvent('#questions-form__header__navigation__step-3', 'fa-pencil-square-o', 'fa-pencil-square');
   registerStepHoverEvent('#questions-form__header__navigation__step-4', 'fa-envelope-o', 'fa-envelope');
-
-  console.log('loaded questions..');
-
-  // fit step label text to container
-  // $('.step-label').find('h3').each(function() {
-  //   $(this).fitText();
-  // })
 
   addClickHandlersForClassName(handleStepClick, 'questions-form__header__navigation__step__content');
   addClickHandlersForClassName(handleExpandArrowClick, 'expand-arrow');
